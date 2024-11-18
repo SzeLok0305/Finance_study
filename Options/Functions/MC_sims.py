e03f14300b09e386f94a07588529ea3d554e6a5f
Lok2047 <szell0305@gmail.com>
2024-11-19 02:24:18 +0800

    new option pricing method

---
+++ Options/Functions/MC_sims.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import datetime
+
+def MC_simulation(df,step,N_walk,simple_mode=True):
+    D_X1 = (df/df.shift(1)).dropna()
+    last_date = df.index[-1]
+    date_time = [last_date+datetime.timedelta(j) for j in range(step+1)]
+    sim_pd = pd.DataFrame()
+    Walks = []
+    for _ in range(N_walk):
+        walk = Random_Walk(D_X1, step)
+        Walks.append(walk)
+    
+    Walks = np.array(Walks)
+    Walks = np.cumprod(Walks,axis=1)
+    if simple_mode:
+        sim_pd = sim_pd*df.iloc[-1]
+        sim_pd['walk_mean'] = Walks.mean(axis=0)*df.iloc[-1]
+        sim_pd['walk_median'] = np.median(Walks, axis=0) * df.iloc[-1]
+        sim_pd['1_sigma_lower'] = np.quantile(Walks, 0.159, axis=0) * df.iloc[-1]
+        sim_pd['1_sigma_upper'] = np.quantile(Walks, 0.841, axis=0) * df.iloc[-1]
+        sim_pd['2_sigma_lower'] = np.quantile(Walks, 0.023, axis=0) * df.iloc[-1]
+        sim_pd['2_sigma_upper'] = np.quantile(Walks, 0.977, axis=0) * df.iloc[-1]
+
+        sim_pd['Date'] = date_time
+        sim_pd = sim_pd.set_index('Date')
+        return sim_pd
+    else:
+        return Walks*df.iloc[-1]
+
+
+
+def Random_Walk(Time_series_return, step):
+    # Generate random samples based on the histogram
+    num_steps = step  # Define the number of steps in the random walk
+    walk = np.random.choice(Time_series_return, size=num_steps, replace=True)
+    walk = np.insert(walk, 0, 1)
+    return walk
