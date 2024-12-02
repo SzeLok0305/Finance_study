f6a05498f87b54e58f1866b8e2551f6cbf0f9a3a
Lok2047 <szell0305@gmail.com>
2024-12-03 00:38:43 +0800

    Update on calculations

--- yfinance_analysis/Functions/AlphaBeta.py
+++ yfinance_analysis/Functions/AlphaBeta.py
@@ -8,26 +8,32 @@ Created on Thu Aug 29 22:19:54 2024
 from matplotlib import pyplot as plt
 import numpy as np
 
-def AlphaBeta(strategy,market,risk_free_rate,plot=False):
-    
-    x,y = strategy, market
-    
+def AlphaBeta(strategy, market, plot=False):
+    x, y = market, strategy
     
     coefficients = np.polyfit(x, y, deg=1)
     
     beta = coefficients[0]
-    beta = np.round(beta,decimals=3)
+    beta = np.round(beta, decimals=3)
     
     alpha = coefficients[1]
-    alpha = np.round(alpha,decimals=3)
-    if plot:
-        
-        y_pred = np.polyval(coefficients, x)
+    alpha = np.round(alpha, decimals=3)
     
+    if plot:
+        y_pred = np.polyval(coefficients, x)
+        
         plt.scatter(x, y, label='Original Data')
         plt.plot(x, y_pred, color='red', label='Linear Regression')
-        plt.xlabel('Strategy')
-        plt.ylabel('Control')
+        text = f'Alpha: {alpha:.2f}\nBeta: {beta:.2f}'
+        plt.text(0.2, 0.8, text,
+             transform=plt.gca().transAxes,
+             verticalalignment='top',
+             horizontalalignment='right',
+             bbox=dict(boxstyle='round', facecolor='white', alpha=0.8))
+        plt.xlabel('Market Return')  # Changed label
+        plt.ylabel('Strategy Return')  # Changed label
         plt.legend()
+        plt.grid(True)
         plt.show()
+        
     return alpha, beta
